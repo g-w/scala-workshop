9e384aa79acf56740040996cdce73f7e2ce5f1a0
Christian <christian.iwanzik@gmail.com>
2016-03-05 16:35:06 +0100

    Description for the companion object

--- src/main/scala/section2/2_companion_object.scala
+++ src/main/scala/section2/2_companion_object.scala
@@ -1,13 +1,14 @@
 package section2
 
-import java.util.Calendar
-
 /********************
  * COMPANION OBJECTS
  *******************/
 
 // Möchte man nun ein statisches Element für alle Instanzen einer Klasse einführen,
 // so bietet Scala die Möglichkeit eines companion objects.
+// Hat ein object denselben Namen wie eine class, so hängen diese beiden Elemente
+// zusammen. So kann eine Instanz einer class auf die private member eines objects
+// zugreifen und umgekehrt.
 
 /**
   *
@@ -26,6 +27,12 @@ class Car(var colour: String, age: Int) {
     fuel = fuel + liter
   }
 
+  /**
+    * Diese Methode ist `private` und lässt dieses Auto
+    * mit einem anderen Auto zusammenstoßen.
+    *
+    * @param other
+    */
   private def crashWith(other: Car) {
     this.explodeAirbags()
     other.explodeAirbags()
@@ -37,7 +44,9 @@ class Car(var colour: String, age: Int) {
 }
 
 /**
-  *
+  * Dies ist das Companion Object. Es hat den selben Namen wie die obere class.
+  * Somit kann in Methode `startTraffic` auf die `crashWith` Methode der übergebenen
+  * `Car` Instanzen zugreifen.
   */
 object Car {
   def startTraffic(cars: Car*) = {
@@ -46,16 +55,27 @@ object Car {
     }
   }
 
+  /**
+    * Ein weiterer Einsatzzweck der Companion objects sind Factorymethoden.
+    * Scala bietet hierfür die spezielle Methode `apply` an. Dies erlaubt
+    * eine Kurzschreibweise beim Aufrufen, wie wir weiter unten sehen.
+    *
+    * @param color
+    * @param age
+    * @return
+    */
   def apply(color: String, age: Int): Car = {
     new Car(color, age)
   }
 }
 
 
-
+/**
+  * Für einen Test erzeugen wir uns kurzerhand eine `main` Methode in einem statischen `Main` Object:
+  */
 object Main {
   def main(args: Array[String]) = {
-    val firstCar = Car("blue", 3)
+    val firstCar = Car("blue", 3)   // Hier wird eigentlich Car.apply("blue", 3) aufgerufen.
     val secondCar = Car("red", 5)
 
     Car.startTraffic(firstCar, secondCar)
